cca1813919ad26306acdef17b28bf2db0bffb932
centurionmars <resident4f@gmail.com>
2022-06-10 00:50:45 +0430

    add nav menus

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@ define('EXP_PATH', get_template_directory());
 define('EXP_URL', get_template_directory_uri());
 function expert_add_assets()
 {
-    wp_register_style('experts-main-style', EXP_URL. '/assests/css/main.css');
+    wp_register_style('experts-main-style', EXP_URL. '/assets/css/main.css');
     wp_enqueue_style('experts-main-style');
     if (is_single())
     {
-        wp_register_script('expert-main-script', EXP_URL. '/assests/js/main.js', ['jquery'],'1.0.0', true);
+        wp_register_script('expert-main-script', EXP_URL. '/assets/js/main.js', ['jquery'],'1.0.0', true);
         wp_enqueue_script('expert-main-script');
     }
 
@@ -26,7 +26,7 @@ function menu_item_desc( $item_id, $item ) {
 		<span class="description"><?php _e( "Item Description", 'menu-item-desc' ); ?></span><br />
 		<input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
 		<div class="logged-input-holder">
-			<input type="text" name="menu_item_desc[<?php echo $item_id ;?>]" id="menu-item-desc-<?php echo $item_id ;?>" value="<?php echo esc_attr( $menu_item_desc ); ?>" />
+            <label for="menu-item-desc-<?php echo $item_id ;?>"></label><input type="text" name="menu_item_desc[<?php echo $item_id ;?>]" id="menu-item-desc-<?php echo $item_id ;?>" value="<?php echo esc_attr( $menu_item_desc ); ?>" />
 		</div>
 	</div>
 	<?php
@@ -48,13 +48,13 @@ function show_menu_item_desc( $title, $item ) {
 	}
 	return $title;
 }
-add_filter( 'nav_menu_item_title', 'show_menu_item_desc', 10, 2 );
-add_action( 'wp_update_nav_menu_item', 'save_menu_item_desc', 10, 2 );
-add_action( 'wp_nav_menu_item_custom_fields', 'menu_item_desc', 10, 2 );
-add_action('wp_enqueue_scripts', 'expert_add_assets');
-add_action('after_setup_theme', 'experts_setup');
+add_filter('nav_menu_item_title'            , 'show_menu_item_desc ', 10, 2 );
+add_action('wp_update_nav_menu_item'        , 'save_menu_item_desc ', 10, 2 );
+add_action('wp_nav_menu_item_custom_fields' , 'menu_item_desc      ', 10, 2 );
+add_action('wp_enqueue_scripts'             , 'expert_add_assets   ');
+add_action('after_setup_theme'              , 'experts_setup       ');
 
-include EXP_PATH . "/includes/front/function.php";
-include EXP_PATH . "/includes/front/post-types.php";
-include EXP_PATH . "/includes/front/taxonamies.php";
-include EXP_PATH . "/includes/front/menus.php";
+include sprintf( "%s/includes/front/function.php", EXP_PATH );
+include sprintf( "%s/includes/front/post-types.php", EXP_PATH );
+include sprintf( "%s/includes/front/taxonomies.php", EXP_PATH );
+include sprintf( "%s/includes/front/menus.php", EXP_PATH );
